395ec7ae90b5c2627c183e7bbb2aa948823df02f
Konrad Mattheis <konrad.mattheis@analytics2go.net>
2019-01-13 12:02:02 +0100

    add first test

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace tests
+{
+    #region Usings
+    using System;
+    using Xunit;
+    #endregion
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
